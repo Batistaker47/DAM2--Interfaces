a229e8fe4ffd26df555601c703b981ae27aa7dc0
batistaker47 <david.pires1997@gmail.com>
2024-10-30 08:57:33 +0100

    Calculadora funcional con numeros enteros

--- Primeros Pasos/Calculadora/Form1.vb
+++ Primeros Pasos/Calculadora/Form1.vb
@@ -1,4 +1,9 @@
 ﻿Public Class FormPrincipal
+
+    Private memoria1 As Double
+    Private memoria2 As Double
+    Private signo As String
+
     Private Sub FormPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -11,13 +16,74 @@
     End Sub
 
     Private Sub ButtonSumar_Click(sender As Button, e As EventArgs) Handles ButtonSumar.Click
-        Dim numero As Integer = TextBoxDisplay.Text
-        TextBoxOperations.Text = numero
-        TextBoxDisplay.Text = ""
+        Try
+            If TextBoxDisplay.Text <> "" Then
+                memoria1 = Val(TextBoxDisplay.Text)
+                signo = "+"
+                TextBoxDisplay.Clear()
+            End If
+        Catch ex As Exception
+        End Try
 
     End Sub
 
+
+    Private Sub ButtonMenos_Click(sender As Object, e As EventArgs) Handles ButtonMenos.Click
+        Try
+            If TextBoxDisplay.Text <> "" Then
+                memoria1 = Val(TextBoxDisplay.Text)
+                signo = "-"
+                TextBoxDisplay.Clear()
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub ButtonMult_Click(sender As Object, e As EventArgs) Handles ButtonMult.Click
+        Try
+            If TextBoxDisplay.Text <> "" Then
+                memoria1 = Val(TextBoxDisplay.Text)
+                signo = "*"
+                TextBoxDisplay.Clear()
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+    Private Sub ButtonDiv_Click(sender As Object, e As EventArgs) Handles ButtonDiv.Click
+        Try
+            If TextBoxDisplay.Text <> "" Then
+                memoria1 = Val(TextBoxDisplay.Text)
+                signo = "/"
+                TextBoxDisplay.Clear()
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
     Private Sub ButtonIgual_Click(sender As Button, e As EventArgs) Handles ButtonIgual.Click
+        Try
+            If TextBoxDisplay.Text <> "" And memoria1 <> 0.0 Then
+                memoria2 = TextBoxDisplay.Text
+                Calcular()
 
+            End If
+
+        Catch ex As Exception
+
+        End Try
     End Sub
+
+    Private Sub Calcular()
+        Select Case signo
+            Case "+"
+                TextBoxDisplay.Text = memoria1 + memoria2
+            Case "-"
+                TextBoxDisplay.Text = memoria1 - memoria2
+            Case "*"
+                TextBoxDisplay.Text = memoria1 * memoria2
+            Case "/"
+                TextBoxDisplay.Text = memoria1 / memoria2
+        End Select
+    End Sub
+
+
 End Class
